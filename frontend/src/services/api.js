97d058ceadffa3b04b171adb68bdeb8695c91fdd
Joshua <joshua.christian000@gmail.com>
2026-03-11 12:51:06 +0700

    add movie app project

---
+++ frontend/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "627fc27f092202e64374d626dd24c5fa"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
